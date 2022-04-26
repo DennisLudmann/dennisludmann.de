9f7af0167d024ca9783365b5ff7d5fed2082946f
DennisLudmann <dennisludmann@gmail.com>
2022-04-26 13:28:52 +0200

    bug fix imort setInterval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { setInterval } from 'timers';
+
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,7 @@ import { setInterval } from 'timers';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {   // get a file from angular core (a predefined function) - it's "onload"
-images = ['4.jpg', '7.jpg', '8.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg']
+images = ['4.jpg', '7.jpg', '8.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'];
 currentImage = 0;
 
 ngOnInit() {
