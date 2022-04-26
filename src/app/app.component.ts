182c6f40ddd00794b22850a4be035c9a7d830e6b
DennisLudmann <dennisludmann@gmail.com>
2022-04-26 10:39:40 +0200

    init and img interval set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-dennisludmann';
+export class AppComponent implements OnInit {   // get a file from angular core (a predefined function) - it's "onload"
+images = ['4.jpg', '7.jpg', '8.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg']
+currentImage = 0;
+
+ngOnInit() {
+    this.updateImage();
+}
+
+updateImage(){
+  setInterval(() => {
+    this.currentImage++;
+    this.currentImage = this.currentImage % this.images.length;   // looping through the images
+  } ,8000);
+}
 }
