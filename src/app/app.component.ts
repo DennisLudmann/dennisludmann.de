ad28333e4ada1c23bb6301b463452638d03d548e
DennisLudmann <dennisludmann@gmail.com>
2022-04-28 12:35:26 +0200

    dynamic headlines - still needs filling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {   // get a file from angular core (a predefined function) - it's "onload"
 images = ['4.jpg', '7.jpg', '8.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'];
+headlines =[
+  'Programming is a skill best acquired by practice and example rather than from books',
+  'Programming is a skill best acquired by practice and example rather than from books2',
+  'Programming is a skill best acquired by practice and example rather than from books3',
+  'Programming is a skill best acquired by practice and example rather than from books4',
+  'Programming is a skill best acquired by practice and example rather than from books5',
+  'Programming is a skill best acquired by practice and example rather than from books6',
+  'Programming is a skill best acquired by practice and example rather than from books7',
+];
 currentImage = 0;
 showImage = true;
 
