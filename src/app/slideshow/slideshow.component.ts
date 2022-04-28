246a873679b260820da0caadf38fea8167049cdd
DennisLudmann <dennisludmann@gmail.com>
2022-04-28 20:51:00 +0200

    quotes order and one added

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -10,12 +10,13 @@ export class SlideshowComponent implements OnInit {   // get a file from angular
   images = ['4.jpg', '7.jpg', '8.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg'];
   headlines =[
     'Programming is a skill best acquired by practice and example rather than from books',
-    'Programming isnt about what you know; its about what you can figure out',
+    'The best error message is the one that never shows up',
     'The only way to learn a new programming language is by writing programs in it',
     'Sometimes its better to leave something alone, to pause, and thats very true of programming',
     'Testing leads to failure, and failure leads to understanding',
-    'The best error message is the one that never shows up',
+    'Programming isnt about what you know; its about what you can figure out',
     'The most damaging phrase in the language is.. its always been done this way',
+    'Dont write better error messages, write code that doesnt need them'
   ];
   currentImage = 0;
   showImage = true;
